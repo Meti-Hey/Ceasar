393b1430aed1f3da0384475b9ce7c18246679b76
thaqime <mehmetthqim@gmail.com>
2024-11-08 10:38:16 +0100

    p

---
+++ src/Caesar.java
@@ -0,0 +1,29 @@
+public class Caesar {
+    private String Kt;
+    private int S;
+    private String gt;
+        public Caesar(){
+
+        }
+        public void verschluesseln(){
+
+        }
+        public void entschluesseln(){
+
+    }
+        public int getASCII (char pWert){
+            return (char)pWert;
+    }
+        public char getChar(int pWert){
+            return (char)pWert;
+        }
+        public void setKt (String pWert){
+
+        }
+        public String getKt (){
+          return (String) Kt;
+        }
+        public void getS (int pWert){
+
+        }
+}
