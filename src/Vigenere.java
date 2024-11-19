d5ed6cb7600046a73c44006e6b75983cc638378d
thaqime <mehmetthqim@gmail.com>
2024-11-19 13:25:20 +0100

    Richtig

---
+++ src/Vigenere.java
@@ -0,0 +1,40 @@
+public class Vigenere {
+
+    private String gt;
+
+private String kt;
+
+private String schluessel;
+public Vigenere(){
+
+}
+public void verschluesseln(){}
+
+public void entschluesseln(){
+
+}
+private char zahlenZuBuchstaben(int pWert){
+
+}
+private int buchstabenZuZahlen(char pWert){
+
+}
+public String getGt(){
+return gt;
+}
+public void setGt(String pGt){
+
+}
+public String getKt(){
+ return kt;
+}
+public void setKt(String pGt){
+
+}
+public String getSchluessek(){
+
+}
+public void setSchluessel(String pSchluessel){
+
+}
+}
